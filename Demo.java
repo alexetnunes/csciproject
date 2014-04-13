6ad66f27cdd84a6feda55c113453a1a79920d8e5
alexetnunes <alex.et.nunes@gmail.com>
2014-04-13 17:08:36 -0300

    e

--- Demo.java
+++ Demo.java
@@ -1,6 +1,6 @@
  import javax.swing.*;
 public class Demo {
       public static void main(String [] args){
-     EncryptorWindow ew = new EncryptorWindow();
+    	  EncryptorWindow ew = new EncryptorWindow();
       }
 }
